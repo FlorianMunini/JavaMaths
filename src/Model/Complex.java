a841c306a016c59304b2983882d3c871c8bcafdd
tarpi <pitardpro@gmail.com>
2016-12-08 12:49:56 +0100

    modification du complex

--- src/Model/Complex.java
+++ src/Model/Complex.java
@@ -152,17 +152,4 @@ package Model;
 	        return Objects.hash(re, im);
 	   
 	    }
-	    
-	    public double module()
-	    {
-	    	return Math.sqrt((this.getRe())*(this.getRe())+(this.getIm()*(this.getIm())) );
-	    }
-
-		public double getRe() {
-			return re;
-		}
-
-		public double getIm() {
-			return im;
-		}
 }
